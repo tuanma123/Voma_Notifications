3035e06e55fcee4dabab89daec0e232af3e89af9
ttvo1022 <tonyt.vo22@gmail.com>
2017-03-27 11:16:17 -0700

    Delete FileIOTesting.py

--- Testing Package/FileIOTesting.py
+++
@@ -1,7 +0,0 @@
-import Spammer
-
-list = Spammer.parse_message("/media/tonyvo/VO/Programming/git-repos/Voma_Notifications/Messages/2DayWarning.mv")
-dictionary = {"[NAME]" : "Tuan","[PRICE]" : "600"}
-
-print list
-print(Spammer.construct_token_string(list, dictionary))
